23e8844cc04b84f80777d9405792b10c9768caef
satya reddy <satyareddy1725@gmail.com>
2023-01-28 16:22:24 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Heading from './components/Heading';
+import Section1 from './components/Section1';
+import Section2 from './components/Section2';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <Section1 />
+      <Section2 />
     </div>
   );
 }
---
+++ src/components/Section2.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function section2() {
+  return (
+    <div>section2</div>
+  )
+}
